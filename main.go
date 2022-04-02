da47013683f25134d117663dba71e4ba7981c0ea
117503445 <t117503445@gmail.com>
2022-04-02 22:50:32 +0800

    ci: release

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/spf13/viper"
 )
 
-const version = "1.0.5"
+const version = "1.0.6"
 
 func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "{\"code\": 0, \"message\": \"Hello World\", \"data\": {\"version\": \"%s\", \"date\": \"%s\"}}", version, time.Now())
