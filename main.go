f9b66ca33bc7b02568be60064130eab94bc07d3e
117503445 <t117503445@gmail.com>
2022-04-02 17:58:06 +0800

    1.0.2

--- main.go
+++ main.go
@@ -4,15 +4,28 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+
+	"github.com/spf13/viper"
 )
 
-const version = "1.0.1"
+const version = "1.0.2"
 
 func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "{\"code\": 0, \"message\": \"Hello World\", \"data\": {\"version\": \"%s\", \"date\": \"%s\"}}", version, time.Now())
 }
 
+func loadConfig() {
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
+	loadConfig()
+
 	http.HandleFunc("/", hello)
 	addr := ":8080"
 	fmt.Printf("server is listening in %s\n", addr)
